e5bc847b7a96cf0d070c90107a9577c0130c8e84
sumanthanumula <anumula.su@northeastern.edu>
2025-07-25 23:51:28 -0400

    Initial Config, Data Indestion, Data Preprocessing

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sentiment-analysis
 Reddit Comments Sentiment Analysis with ML FLOW
+
+### Start App
+```python -m venv venv```
+```source venv/bin/activate```
+```pip install -r requirements.txt```
+```dvc init```
+```dvc repro```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'sentiment_analysis',
+    version= '0.0.0',
+    author= 'Sumanth',
+    author_email= 'anumulasumanth@yahoo.com',
+    packages= find_packages(),
+    install_requires= [],
+    python_requires='>=3.10,<3.13',
+)
